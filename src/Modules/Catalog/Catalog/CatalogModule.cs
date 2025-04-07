438c06c8050bf312f686c11c579a0492c71dd2b8
Rafael Angelino <rafaelstringuetti@gmail.com>
2025-04-07 06:51:37 -0300

    feat: desenvolvendo shared e catalog data infrastructure com ef core code first

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,15 +9,41 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
-            //services
-            //    .AddApplicationServices()
-            //    .AddInfrastructureServices(configuration)
-            //    .AddApiServices(configuration);
+            //add services to the container
+
+            //api endpoint services
+
+            //application use case services
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            //data - Infrastructure services
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            //configure the HTTP request pipeline   
+
+            //1 Use api endpoint services   
+            //2 Use application use case services   
+            //3 Use data - Infrastructure services
+
+            app.UseMigration<CatalogDbContext>();
             return app;
         }
     }
---
+++ src/Modules/Catalog/Catalog/Data/Seed/InitialData.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Data.Seed
+{
+    public static class InitialData
+    {
+        public static IEnumerable<Product> Products =>
+        new List<Product>
+        {
+            Product.Create(Guid.NewGuid(), "Iphone 16", ["Category 1"], "Description 1", "ImageFile", 500),
+            Product.Create(Guid.NewGuid(), "Samsung S25", ["Category 2"], "Description 2", "ImageFile", 400),
+            Product.Create(Guid.NewGuid(), "HyperV", ["Category 3"], "Description 3", "ImageFile", 300)
+        };
+    }
+}
